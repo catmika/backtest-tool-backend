337e1357a17676bcd5e34585ed103796df20416f
Maksym Andriiash <maksym.andriiash@kultrix.com>
2024-05-12 16:36:01 +0300

    [update] symbols migration, model, get method, mapper, added twelve data base url to .env

--- src/routes/symbols/service.ts
+++ src/routes/symbols/service.ts
@@ -20,7 +20,7 @@ const getSymbols = async (params: IPaginationObject) => {
           { shortName: regex },
           { fullName: regex },
           { market: regex },
-          { exchange: regex },
+          { exchanges: { $in: [regex] } },
         ],
       },
     });
@@ -36,14 +36,14 @@ const getSymbols = async (params: IPaginationObject) => {
 
   if (!params.page || !params.limit) {
     const symbol = await Symbol.aggregate([...pipelines]);
-    return symbol.map(mapSymbol);
+    return { results: symbol.map(mapSymbol) };
   }
 
   const offset = (+params.page - 1) * +params.limit;
 
   const results = await Symbol.aggregate(
     [
-      // ...pipelines,
+      ...pipelines,
       {
         $facet: {
           paginatedResults: [{ $skip: offset }, { $limit: +params.limit }],
@@ -66,7 +66,7 @@ const getSymbols = async (params: IPaginationObject) => {
   const symbol = results[0].paginatedResults;
 
   return {
-    data: symbol.map(mapSymbol),
+    results: symbol.map(mapSymbol),
     totalCount: results?.[0]?.totalCount?.[0]?.count || 0,
   };
 };
